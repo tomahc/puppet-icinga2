b82eacff18b2c02118896ee64e4ad5fdb620543a
Daniel Chamot <daniel.chamot@inovex.de>
2015-11-04 09:17:30 +0100

    hash2ic rewritten

--- lib/puppet/parser/functions/hash2ic.rb
+++ lib/puppet/parser/functions/hash2ic.rb
@@ -22,25 +22,29 @@ Would return:
     puppethash = arguments[0]
     storage = arguments[1]
 
-    puppethash.each do |k,v|
-        key = "\"#{k}\" ="
-        if storage.empty?
-            storage.push '{ '
-        end
+    hash2ic = Proc.new do |arg1,arg2|
+      is_first = true
+      arg2.push '{ '
 
-        storage.push key
+      arg1.each do |k,v|
+      key = "\"#{k}\" ="
+      if not is_first
+        arg2.push ', '
+      end
 
-        if v.is_a? Hash
-            storage.push '{ '
-            hash2ic v, storage
-
-        elsif v.is_a? Array
-            val = v.to_s + ' }' + ' ,'
-        else
-            val = "\"#{v}\"" + ' }' + ' ,'
-        end
-        storage.push val
+      arg2.push key
+      if v.is_a? Hash
+        hash2ic.call(v, arg2)
+      elsif v.is_a? Array
+        arg2.push v.to_s
+      else
+        val = "\"#{v}\""
+        arg2.push val
+      end
+      is_first = false
     end
-    return storage + ['}']
+      arg2.push '} '
+    end
+    return hash2ic.call(puppethash,storage)
   end
 end
