465af320932b7990e72498bb83e27253d5bd6f39
Daniel Chamot <daniel.chamot@inovex.de>
2015-11-04 17:54:22 +0100

    added doc and did some linting

--- lib/puppet/parser/functions/hash2ic.rb
+++ lib/puppet/parser/functions/hash2ic.rb
@@ -5,14 +5,16 @@
 
 module Puppet::Parser::Functions
   newfunction(:hash2ic, :type => :rvalue, :doc => <<-EOS
-Returns a icinga2 hash
+Returns a icinga2 dictionary/hash
 *Examples:*
 
-    makedirs('/tmp/somedir','a/deeper/dir')
+    hash2ic( { 'key' => 'value', 'nested' => { 'key' => 'value' } }, [] )
 
 Would return:
 
-    ['/tmp/somedir','/tmp/somedir/a', '/tmp/somedir/a/deeper', ... aso.. ]
+    { "key" = "value", "nested" = { "key" = "value" } }
+
+    ...which is icing2s' dictionary/hash syntax
     EOS
   ) do |arguments|
 
@@ -28,6 +30,7 @@ Would return:
 
       arg1.each do |k,v|
       key = "\"#{k}\" ="
+
       if not is_first
         arg2.push ', '
       end
@@ -35,16 +38,21 @@ Would return:
       arg2.push key
       if v.is_a? Hash
         hash2ic.call(v, arg2)
+
       elsif v.is_a? Array
         arg2.push v.to_s
+
       else
         val = "\"#{v}\""
         arg2.push val
       end
+
       is_first = false
     end
+
       arg2.push '} '
     end
+
     return hash2ic.call(puppethash,storage)
   end
 end
