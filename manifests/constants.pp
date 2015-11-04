d7e48958bdb01c1c1e712e093a18007fc3b19d87
Daniel Chamot <daniel.chamot@inovex.de>
2015-11-04 19:51:44 +0100

    constants

---
+++ manifests/constants.pp
@@ -0,0 +1,27 @@
+class icinga2::constants (
+  $ensure,
+
+  $plugindir            = undef,
+  $manubulonplugindir   = undef,
+  $plugincontribdir     = undef,
+  $zonename             = undef,
+  $ticketsalt           = undef,
+
+  $service              = $icinga2::server::service,
+) {
+
+  $real_ensure = $ensure ? {
+    /(true|present|file)/ => 'file',
+    /(false|absent)/      => 'absent',
+    default               => fail("No such option: ${ensure}"),
+  }
+
+  file { "${icinga2::params::confdir}/constants.conf":
+    ensure  => $real_ensure,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('icinga2/server/constants.conf.erb'),
+    notify  => Service[$service],
+  }
+}
