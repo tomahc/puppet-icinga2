9d2027f59a3cf6d43d30e02a027f153928cd66fc
Daniel Chamot <daniel.chamot@inovex.de>
2015-11-06 10:10:51 +0100

    endpoints

---
+++ manifests/endpoint.pp
@@ -0,0 +1,32 @@
+define icinga2::endpoint (
+  $ensure,
+
+  $host                     = undef,
+  $port                     = undef,
+
+  $zone_configs             = $icinga2::server::zone_configs,
+  $service                  = $icinga2::server::service,
+
+) {
+
+  $real_ensure = $ensure ? {
+    /(true|present|file)/ => 'file',
+    /(false|absent)/      => 'absent',
+    default               => fail("No such option: ${ensure}"),
+  }
+
+  $real_configs = $is_template ? {
+    true    => $icinga2::server::template_configs,
+    false   => $zone_configs,
+    default => fail("No such option: ${is_template}"),
+  }
+
+  file { "${icinga2::params::confdir}/${real_configs}/${name}.conf":
+    ensure  => $real_ensure,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('icinga2/objects/zone.conf.erb'),
+    notify  => Service[$service],
+  }
+}
