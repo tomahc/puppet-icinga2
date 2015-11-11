74688f5b995fee18a5f702168048fba41870d22b
Daniel Chamot <daniel.chamot@inovex.de>
2015-11-11 10:40:07 +0100

    changed exec condition in feature

--- manifests/feature.pp
+++ manifests/feature.pp
@@ -12,7 +12,7 @@ define icinga2::feature (
   exec { "${real_ensure}-${name}":
     command     => "icinga2 feature ${real_ensure} ${name}",
     path        => ['/usr/sbin', '/usr/bin'],
-    unless      => ["test -e /etc/icinga2/features-enabled/${name}.conf"],
+    onlyif      => ["test -e /etc/icinga2/features-enabled/${name}.conf"],
     notify      => Service[$icinga2::server::service],
   }
 }
